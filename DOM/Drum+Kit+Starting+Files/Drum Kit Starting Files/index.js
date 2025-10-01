9c7d5f9c9530d0870bce310cb09287138b15ca92
salwin <salwinsolomon12345@gmail.com>
2025-10-01 16:33:03 +0530

    learning

---
+++ DOM/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,9 @@
+
+for (var i=0 ; i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        alert ("Button "+this.innerHTML+" clicked!");
+    });
+    
+
+}
+
