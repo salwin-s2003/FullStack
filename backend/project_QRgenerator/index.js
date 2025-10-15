a1f0c4ced73693eb917fa90f1b877f0f53042dcc
salwin <salwinsolomon12345@gmail.com>
2025-10-15 10:07:23 +0530

    project qr generator

---
+++ backend/project_QRgenerator/index.js
@@ -0,0 +1,27 @@
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
+inquirer
+  .prompt([
+    {message:"Type the URL:",
+        name:"URL",
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_image.png'));
+
+    fs.writeFile('url.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The URL has been saved to url.txt!');
+      });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
